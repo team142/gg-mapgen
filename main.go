ac530a922ee348a5bfa5050db58b8a5b0c95b826
Bjorn <bjorn5490@gmail.com>
2018-02-05 15:38:54 +0200

    Changed Map Workings

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/aquilax/go-perlin"
 	"github.com/nsf/termbox-go"
 	// "time"
 	// "math/rand"
-	"encoding/json"
+
 	"math"
 	"strconv"
 	"strings"
@@ -20,7 +21,7 @@ import (
 )
 
 var basePath = "/"
-var baseListener = ":8080"
+var baseListener = ":8081"
 
 type Tile struct {
 	X, Y, TilePrint int
@@ -44,23 +45,23 @@ type MapsWatch struct {
 var allMaps = MapsWatch{}
 
 const (
-	// alpha       = 2.
-	// beta        = 2.
-	// n           = 3
-	// div 		= 50
+	alpha = 2.
+	beta  = 2.
+	n     = 3
+	div   = 50
 	// seed  int64 = 100
 
 	defaultHeight = 50
 	defaultWidth  = 50
 )
 
-func getMap(mapId int, width int, height int, startX int, startY int) {
+func getMap(responseWriter http.ResponseWriter, mapSeed int64, width int, height int, startX int, startY int) {
 
-	theMap, ok := allMaps.Maps[mapId]
-	if !ok {
-		//Clean Up
-		panic("Map does not exist")
-	}
+	// theMap, ok := allMaps.Maps[mapID]
+	// if !ok {
+	// 	//Clean Up
+	// 	panic("Map does not exist")
+	// }
 
 	// check if we have generated this part of the map before
 	w, h := termbox.Size()
@@ -68,73 +69,78 @@ func getMap(mapId int, width int, height int, startX int, startY int) {
 	w = width + startX
 	h = height + startY
 
-	p := perlin.NewPerlin(theMap.alpha, theMap.beta, theMap.n, theMap.seed)
+	p := perlin.NewPerlin(alpha, beta, n, mapSeed)
 	_, _, _ = w, p, strconv.Itoa
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 	for y := startY; y < h; y++ {
 		for x := startX; x < w; x++ {
 
-			tileId := strconv.FormatInt(int64(x), 10) + ":" + strconv.FormatInt(int64(y), 10)
-			tile, okT := theMap.Tiles[tileId]
+			// tileID := strconv.FormatInt(int64(x), 10) + ":" + strconv.FormatInt(int64(y), 10)
+			// tile, okT := theMap.Tiles[tileID]
 			char := rune(' ')
 
-			if !okT {
-				tile = Tile{}
+			// if !okT {
+			tile := Tile{}
 
-				noiseFloat := p.Noise2D(float64(x)/theMap.div, float64(y)/theMap.div)
-				noise := noiseFloat
-				os.Getenv("GG_MAP_PATH")
-				noiseFloat = math.Abs(noiseFloat*theMap.div) + 1
-				// fmt.Printf("%0.0f\t%0.0f\t%0.4f\n", x, y, noise)
-				// noise = noise%8
+			noiseFloat := p.Noise2D(float64(x)/div, float64(y)/div)
+			noise := noiseFloat
+			os.Getenv("GG_MAP_PATH")
+			noiseFloat = math.Abs(noiseFloat*div) + 1
+			// fmt.Printf("%0.0f\t%0.0f\t%0.4f\n", x, y, noise)
+			// noise = noise%8
 
-				if noiseFloat <= 1 {
-					//water
+			if noiseFloat <= 1 {
+				//water
 
-					tile.TilePrint = 1
+				tile.TilePrint = 1
 
-					noiseFloat = 75
-					//Blue
-				} else if noiseFloat > 1 && noiseFloat <= 4 {
-					//Beach
-					tile.TilePrint = 2
-					noiseFloat = 70
-					//Green
-				} else if noiseFloat > 4 && noiseFloat <= 6 {
-					// 1 off beach
-					tile.TilePrint = 2
-					noiseFloat = 145
-				} else if noiseFloat > 6 && noiseFloat <= 7 {
-					//Mountanous
-					tile.TilePrint = 3
-					noiseFloat = 78
-				} else if noiseFloat > 7 && noiseFloat <= 15 {
-					noiseFloat = 3
-					tile.TilePrint = 3
-				} else if noiseFloat > 15 && noiseFloat <= 25 {
-					noiseFloat = 23
-					tile.TilePrint = 3
-				} else if noiseFloat > 25 && noiseFloat <= 50 {
-					noiseFloat = 250
-					tile.TilePrint = 3
-				} else {
-					//Mountain
-					tile.TilePrint = 4
-					noiseFloat = 250
-				}
-				_ = char
-
-				_ = termbox.SetOutputMode(termbox.Output256)
-
-				tile.X = x
-				tile.Y = y
-				tile.Type = int(noiseFloat)
-				// tile.TilePrint = 1
-				tile.NoiseValue = noise
-				theMap.Tiles[tileId] = tile
-				// termbox.SetCell(x, y, char, termbox.Attribute(y), termbox.Attribute(tile.Type))
-
+				noiseFloat = 75
+				//Blue
+			} else if noiseFloat > 1 && noiseFloat <= 4 {
+				//Beach
+				tile.TilePrint = 2
+				noiseFloat = 70
+				//Green
+			} else if noiseFloat > 4 && noiseFloat <= 6 {
+				// 1 off beach
+				tile.TilePrint = 2
+				noiseFloat = 145
+			} else if noiseFloat > 6 && noiseFloat <= 7 {
+				//Mountanous
+				tile.TilePrint = 3
+				noiseFloat = 78
+			} else if noiseFloat > 7 && noiseFloat <= 15 {
+				noiseFloat = 3
+				tile.TilePrint = 3
+			} else if noiseFloat > 15 && noiseFloat <= 25 {
+				noiseFloat = 23
+				tile.TilePrint = 3
+			} else if noiseFloat > 25 && noiseFloat <= 50 {
+				noiseFloat = 250
+				tile.TilePrint = 3
+			} else {
+				//Mountain
+				tile.TilePrint = 4
+				noiseFloat = 250
 			}
+			_ = char
+
+			_ = termbox.SetOutputMode(termbox.Output256)
+
+			tile.X = x
+			tile.Y = y
+			tile.Type = int(noiseFloat)
+			tile.NoiseValue = noise
+			// theMap.Tiles[tileID] = tile
+
+			// }
+
+			out, err := json.Marshal(tile)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Fprintf(responseWriter, "%s", out)
+
 			termbox.SetCell(x, y, char, termbox.Attribute(y), termbox.Attribute(tile.Type))
 		}
 	}
@@ -143,15 +149,12 @@ func getMap(mapId int, width int, height int, startX int, startY int) {
 
 func generateNewMap() {
 	//Get new Map ID
-	newMap := Map{2, 2, 3, 50, 1, map[string]Tile{}}
-	newMap.Tiles = make(map[string]Tile)
+	// newMap := Map{2, 2, 3, 50, 1, map[string]Tile{}}
+	// newMap.Tiles = make(map[string]Tile)
 
-	max := len(allMaps.Maps)
-	// fmt.Println(max)
-	// panic("NO")
-	allMaps.Maps[max+1] = newMap
-
-	getMap(max+1, defaultWidth, defaultHeight, 0, 0)
+	// max := len(allMaps.Maps)
+	// allMaps.Maps[max+1] = newMap
+	// getMap(max+1, defaultWidth, defaultHeight, 0, 0)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -161,38 +164,52 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		uri = strings.Replace(uri, basePath[1:], "", -1)
 	}
 
-	mapId, err := strconv.Atoi(uri)
-	if err != nil {
-
-		// out, err := json.Marshal(allMaps)
-		// if err != nil {
-		// 	panic (err)
-		// }
-		// fmt.Fprintf(w, "%s", out)
-
-		fmt.Fprintf(w, "Cannot use: %s!", r.URL.Path[1:])
-		// fmt.Println(i1)
-	} else {
-		_, ok := allMaps.Maps[mapId]
-		if !ok {
-			//Clean Up
-			// panic("Map does not exist");
-			fmt.Fprintf(w, "No Map with id: %s", r.URL.Path[1:])
-		} else {
-
-			getMap(mapId, defaultWidth, defaultHeight, 0, 0)
-
-			//Clean Up
-			theMap2, _ := allMaps.Maps[mapId]
-
-			out, err := json.Marshal(theMap2)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Fprintf(w, "%s", out)
-		}
-
+	runes := []rune(uri)
+	var mapSeedID = ""
+	for _, i := range runes {
+		mapSeedID += fmt.Sprintf("%d", i)
 	}
+	fmt.Println(mapSeedID)
+
+	mapSeed, err := strconv.Atoi(mapSeedID)
+	if err != nil {
+		mapSeed = 1
+	}
+
+	getMap(w, int64(mapSeed), defaultWidth, defaultHeight, 0, 0)
+
+	// mapID, err := strconv.Atoi(uri)
+	// if err != nil {
+
+	// 	// out, err := json.Marshal(allMaps)
+	// 	// if err != nil {
+	// 	// 	panic (err)
+	// 	// }
+	// 	// fmt.Fprintf(w, "%s", out)
+
+	// 	fmt.Fprintf(w, "Cannot use: %s!", r.URL.Path[1:])
+	// 	// fmt.Println(i1)
+	// } else {
+	// 	_, ok := allMaps.Maps[mapID]
+	// 	if !ok {
+	// 		//Clean Up
+	// 		// panic("Map does not exist");
+	// 		fmt.Fprintf(w, "No Map with id: %s", r.URL.Path[1:])
+	// 	} else {
+
+	// 		getMap(mapID, defaultWidth, defaultHeight, 0, 0)
+
+	// 		//Clean Up
+	// 		theMap2, _ := allMaps.Maps[mapID]
+
+	// 		out, err := json.Marshal(theMap2)
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	// 		fmt.Fprintf(w, "%s", out)
+	// 	}
+
+	// }
 
 }
 
@@ -208,20 +225,20 @@ func main() {
 	// fmt.Println("basePath: ", basePath)
 	// fmt.Println("baseListener: ", baseListener)
 	http.HandleFunc(basePath, handler)
-	allMaps.Maps = make(map[int]Map)
-	newMap := Map{2, 2, 3, 50, 1, map[string]Tile{}}
-	max := 0
+	// allMaps.Maps = make(map[int]Map)
+	// newMap := Map{2, 2, 3, 50, 1, map[string]Tile{}}
+	// max := 0
 	// fmt.Println(max)
-	allMaps.Maps[max+1] = newMap
+	// allMaps.Maps[max+1] = newMap
 	//Clean Up
-	getMap(max+1, defaultWidth, defaultHeight, 0, 0)
+	// getMap(max+1, defaultWidth, defaultHeight, 0, 0)
 
 	if terminal.IsTerminal(int(os.Stdout.Fd())) {
 		go http.ListenAndServe(baseListener, nil)
 	} else {
 		http.ListenAndServe(baseListener, nil)
 		//There is no tty, shut down
-		os.Exit()
+		os.Exit(0)
 	}
 
 	//Everything from here on out, is for termbox
@@ -232,17 +249,17 @@ func main() {
 	}
 	defer termbox.Close()
 
-	event_queue := make(chan termbox.Event)
+	eventQueue := make(chan termbox.Event)
 	go func() {
 		for {
-			event_queue <- termbox.PollEvent()
+			eventQueue <- termbox.PollEvent()
 		}
 	}()
 
 loop:
 	for {
 		select {
-		case ev := <-event_queue:
+		case ev := <-eventQueue:
 			if ev.Type == termbox.EventKey && ev.Key == termbox.KeyEsc {
 				break loop
 			}
